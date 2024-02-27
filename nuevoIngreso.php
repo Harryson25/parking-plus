336340ce7c68930e03da342782a77b221fd8f8b4
Juan Astaiza <astaiza25706131@gmail.com>
2024-02-27 09:49:46 -0500

    exito.php

--- nuevoIngreso.php
+++ nuevoIngreso.php
@@ -1,39 +1,80 @@
 <?php
 $nombrePagina = "Nuevo Ingreso";
-    include 'plantilla.php';
-    include 'header.php';
+include 'plantilla.php';
+include 'header.php';
+
+
+// Verificar si se ha enviado el formulario
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    // Capturar datos del formulario
+    $tipoVehiculo = $_POST["tipoVehiculo"];
+    $marca = $_POST["marca"];
+    $color = $_POST["color"];
+    $placa = $_POST["placa"];
+    $observaciones = $_POST["observaciones"];
+
+    // Realizar la conexión a la base de datos
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $basedatos = "parking_plus_db";
+
+    // Crear una nueva conexión
+    $conexion = new mysqli($servername, $username, $password, $basedatos);
+
+    // Verificar la conexión
+    if ($conexion->connect_error) {
+        die("La conexión a la base de datos tuvo un error: " . $conexion->connect_error);
+    }
+
+    // Armar la consulta SQL para la inserción
+    $insertar = "INSERT INTO vehiculos (tipoVehiculo, marca, color, placa, observaciones)
+    VALUES ('$tipoVehiculo', '$marca', '$color', '$placa', '$observaciones') ";
+
+    // Ejecutar la consulta
+    if ($conexion->query($insertar) === TRUE) {
+        //Redirigir al archivo ezito.php después de la inserción en la DB
+        header("Location: exito.php");
+        exit;
+    } else {
+        echo "Error: " . $insertar . "<br>" . $conexion->error;
+    }
+
+    // Cerrar la conexión
+    $conexion->close();
+}
 ?>
 
-      <div class="contenedor-nuevo-ingreso">
-        <form action="" id="nuevoIngreso"
-            <h3><strong>Información del vehículo:</strong></h3>
 
-            <div class="control-form">
-                <label>Tipo vehículo:</label>
-                <select>
-                    <option value="">Carro</option>
-                    <option value="">Moto</option>
-                    <option value="">Otro</option>
-                </select>
-            </div>
-            <div class="control-form">
-                    <label>Marca:</label>
-                    <input type="text" id="marca"/>
-            </div>
-            <div class="control-form">
-                <label>Color:</label>
-                <input type="text" id="color"/>
-            </div>
-            <div class="control-form">
-                <label>Placa:</label>
-                <input type="text" id="placa"/>
-            </div>
-            <div class="control-form">
-                <label>Observaciones:</label>
-                <input type="text" id="observaciones"/>
-            </div>
-            <button class="botonNuevoVehiculo">Ingresar vehículo</button>
-        </form>
-      </div>
+<div class="contenedor-nuevo-ingreso">
+    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" id="nuevoIngreso" method="post">
+        <h3>Información del Vehículo</h3>
 
-<?php include 'footer.php'; ?>
+        <div class="control-form">
+            <label>Tipo Vehículo</label><br />
+            <select name="tipoVehiculo">
+                <option value="carro">Carro</option>
+                <option value="moto">Moto</option>
+                <option value="otro">Otro</option>
+            </select>
+        </div>
+        <div class="control-form">
+            <label>Marca:</label>
+            <input type="text" id="marca" name="marca" />
+        </div>
+        <div class="control-form">
+            <label>Color:</label>
+            <input type="text" id="color" name="color" />
+        </div>
+        <div class="control-form">
+            <label>Placa:</label>
+            <input type="text" id="placa" name="placa" />
+        </div>
+        <div class="control-form">
+            <label>Observaciones:</label>
+            <input type="text" id="observaciones" name="observaciones" />
+        </div>
+        <button class="botonNuevoVehiculo" type="submit">Ingresar vehiculo</button>
+    </form>
+</div>
