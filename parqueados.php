693d63f744da92d849620dcfe791cb96b7715c79
Juan Astaiza <astaiza25706131@gmail.com>
2024-02-28 11:45:42 -0500

    Trabajando con el buscador de placas

--- parqueados.php
+++ parqueados.php
@@ -1,23 +1,10 @@
 <?php
 
 $nombrePagina = "Ingreso correcto";
-
 include 'plantilla.php';
 include 'header.php';
+include 'conexionbasedatos.php';
 
-// Realizar la conexión a la base de datos
-$servername = "localhost";
-$username = "root";
-$password = "";
-$basedatos = "parking_plus_db";
-
-// Crear una nueva conexión
-$conexion = new mysqli($servername, $username, $password, $basedatos);
-
-// Verificar la conexión
-if ($conexion->connect_error) {
-    die("La conexión a la base de datos tuvo un error: " . $conexion->connect_error);
-}
 
 //Consultar los vehículos parqueados
 $vehiculosParqueados = "SELECT * FROM vehiculos WHERE estado = 'parqueado'";
@@ -28,8 +15,8 @@ $vehiculos = $resultado->fetch_all(MYSQLI_ASSOC);
 
 ?>
 
+<h3 style="padding-left: 2rem;">Vehículos Parqueados</h3>
 <div class="contenedor-listado-parqueados">
-    <h3>Vehículos Parqueados</h3>
 
     <table class="tabla">
         <thead>
